a4f4969dbdbf941cc6ce53dad12b6e9620ea3109
0xdowng <0xdowng@gmail.com>
2025-05-07 17:09:06 +0800

    Update CoinTypeDisplay and ObjectIdDisplay components to use translation for labels, enhancing localization support.

--- packages/client/src/components/CoinManager/DisplayComponents.tsx
+++ packages/client/src/components/CoinManager/DisplayComponents.tsx
@@ -1,27 +1,30 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 import CopyableText from "./CopyableText";
 import { formatCoinType } from "./utils";
 
 // 币种类型显示组件
 export const CoinTypeDisplay: React.FC<{ coinType: string }> = ({ coinType }) => {
+  const { t } = useTranslation();
   return (
     <CopyableText 
       text={coinType} 
       displayText={formatCoinType(coinType)} 
-      label="复制币种类型" 
+      label={t("coinManager.copy.coinType")} 
     />
   );
 };
 
 // 对象ID显示组件
 export const ObjectIdDisplay: React.FC<{ objectId: string }> = ({ objectId }) => {
+  const { t } = useTranslation();
   const displayText = `${objectId.slice(0, 4)}...${objectId.slice(-4)}`;
   
   return (
     <CopyableText 
       text={objectId} 
       displayText={displayText} 
-      label="复制对象ID" 
+      label={t("coinManager.copy.objectId")} 
     />
   );
 }; 
